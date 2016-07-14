0d642935493cba08be0aef07a81c713f232818e4
David Weiss <davidweiss@dnavid.com>
2016-07-14 13:19:23 +0200

    typo

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ sudo apt-get install -y nodejs
 ## curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 ## sudo apt-get install -y nodejs
 sudo apt-get install -y git
-git https://github.com/auth0-samples/auth0-react-sample.git
+git clone https://github.com/auth0-samples/auth0-react-sample.git
 cd auth0-react-sample/00-Starter-Seed/
 
 # Install the dependencies
